510c79710581814b7935624209876f7b6bc1f923
askay-sanjeev <asanjeevkeloth@gmail.com>
2025-08-12 10:04:15 +0530

    timestep choices  updated

--- reboot/utils.py
+++ reboot/utils.py
@@ -6,19 +6,15 @@ plt.rcParams['text.usetex'] = True
 
 def solveFHN(prm):
     def FHN(t, y):
-        u,v = y
+        u,v = y[0], y[1]
         dudt = u*(1-u)*(u-prm['a']) - v
         dvdt = prm['e']*(prm['k']*u-v-prm['b'])
         return [dudt, dvdt]
     
-    t_vec = np.linspace(0, prm['tmax'], prm['tn'])
-    
-    sol = spy.solve_ivp(FHN, (0 , prm['tmax']), prm['y0'], method='RK45' )
+    sol = spyint.solve_ivp(FHN, (0 , prm['tmax']), prm['y0'], method='RK45' )
     return sol.y, sol.t
 
-
 def FHNString(prm):
-    # N = y//2
     def core(t, y):
         u = y[:prm['N']]; v = y[prm['N']:]
         dudt = u*(1-u)*(u-prm['a']) - v 
@@ -30,6 +26,15 @@ def FHNString(prm):
         # dvdt = np.concatenate([np.array(dvdt0), dvdti, np.array(dvdtn)])
         dvdt = np.hstack(([dvdt0], dvdti, [dvdtn]))
         return np.concatenate([dudt,dvdt])
-    
+
     sol = spyint.solve_ivp(core, (0,prm['tmax']), prm['y0'], method='RK45')
     return sol.y, sol.t
+
+def solve_plot(prm):
+    ysol, t = FHNString(prm)
+
+    plt.figure(figsize=(10, 3))
+    plt.imshow(ysol[:prm['N']], cmap='inferno', aspect='auto', interpolation='nearest')
+    cbar = plt.colorbar()   
+    cbar.set_label('$u$')
+    # plt.title(str(prm))
